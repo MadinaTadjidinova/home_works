22242b064312db86d2afd3f4c0af9663d18b970b
Madina Tadjidinova <madina.tadjidinova@gmail.com>
2021-02-09 20:04:32 +0600

    Add js file

---
+++ js-table/scripts.js
@@ -0,0 +1,23 @@
+for (let i1 = 2; i1 <= 9; i1++) {
+   for (let i2 = 2; i2 <= 9; i2++) {
+
+    document.write( "<table><td><th>" + i1 + " * " + i2 + " = " + i1 * i2 + "</th></td></table>" );
+} 
+document.write("<br>")
+}
+
+
+// function example(a, b){
+//     let c = a;
+//     for(let i1 = 0; i1 < b; i1++){
+//         a *=a;
+//     }
+//     return a
+// }
+// return c;
+
+// document.write(example(5, 2));
+// document.write(example(5, 3));
+
+
+//рекусивный метод
