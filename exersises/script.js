e1c99d55f01586366b381f33c3f3898a6877ea1b
Madina Tadjidinova <madina.tadjidinova@gmail.com>
2021-02-09 21:58:41 +0600

    Finisged first examle task

--- exersises/script.js
+++ exersises/script.js
@@ -1,14 +1,33 @@
+//1
 let input1 = document.querySelector('#password');
 let p = document.querySelector('.p');
 
 input1.addEventListener('input', function(event){
     if (input1.value.length == 0 ){
-        div1.innerText = 'Введите пароль';
+        p.innerText = 'Введите пароль';
     }
-    if (input1.value.length > 0 ){
+    else if (input1.value.length > 0 ){
         p.innerText = 'Слабо';
     }
-    if (input1.value.length >= 5 ){
-        p.innerText = 'Хорошо';
+     if (input1.value.length >= 6 ){
+        p.innerText = 'Хорошо'; 
     }
+     if (input1.value.length >= 8 ){
+        p.innerText = 'Очень хорошо';
+     }
 })
+ 
+//2
+let p2 = document.querySelector('.p2')
+
+
+    p2 = Math.random();
+    button = Math.random();
+
+button.addEventListener('click', function(event){
+    p2.innerText = button;
+})
+
+
+let input2 = document.querySelector('#number');
+let button = document.querySelector('button');
