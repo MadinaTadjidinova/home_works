8e3e3b1e650d7883cac76a9f22d2de45b2c8cd65
Madina Tadjidinova <madina.tadjidinova@gmail.com>
2021-02-12 14:03:47 +0600

    Can't move

--- work_wiith-mouse/script.js
+++ work_wiith-mouse/script.js
@@ -1,5 +1,5 @@
 let div = document.querySelector('div')
-document.body.addEventListener('mousedown', function(event){
+document.body.addEventListener('dblclick', function(event){
     div.style.left = event.clientX - 30 + "px"
     div.style.top = event.clientY - 30 + "px"
 }) 
