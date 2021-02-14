86f141d41e95d87aa2976608f54f240b913673ea
Madina Tadjidinova <madina.tadjidinova@gmail.com>
2021-02-14 22:51:16 +0600

    Add html file

--- work_wiith-mouse/script.js
+++ work_wiith-mouse/script.js
@@ -1,4 +1,4 @@
-let div = document.querySelector('div')
+let div = document.createElement('div')
 document.body.addEventListener('dblclick', function(event){
     div.style.left = event.clientX - 30 + "px"
     div.style.top = event.clientY - 30 + "px"
